cd9de3a761be3fcf2cf6ab63345c2f1780914290
Laurent Thomas <laurent132.thomas@laposte.net>
2019-08-12 09:29:12 +0200

    Improve comments

--- NI-USB-6008.py
+++ NI-USB-6008.py
@@ -21,34 +21,34 @@ Pin List
 Num0 = np.array([0], dtype=np.uint8) #  0V
 Num1 = np.array([1], dtype=np.uint8) # +5V
 
-# Create 1 Task object/pin
+# Create 1 Task object per pin
 List_Task  = [BitTask1,BitTask2,VTask1,VTask2] = [Task() for i in range(4)]
 List_Bit   = [BitTask1,BitTask2]
 List_Volt  = [VTask1, VTask2]
 
-# Assign digital Pin ON/OFF 0/+5V
-BitTask1.CreateDOChan("Dev1/port1/line0","BitTask1",DAQmx_Val_ChanPerLine)
-BitTask2.CreateDOChan("Dev1/port1/line1","BitTask2",DAQmx_Val_ChanPerLine)
+# Assign Bit Task to digital pins
+BitTask1.CreateDOChan("Dev1/port1/line0","BitTask1",DAQmx_Val_ChanPerLine) # this is digital pin0 of port1 on Device 1
+BitTask2.CreateDOChan("Dev1/port1/line1","BitTask2",DAQmx_Val_ChanPerLine) # this is digital pin1 of port1 on Device 1
 
-# Assign Analogue Pin for Voltage output
-VTask1.CreateAOVoltageChan("Dev1/ao0","VTask1",0,5,DAQmx_Val_Volts,None)
-VTask2.CreateAOVoltageChan("Dev1/ao1","VTask2",0,5,DAQmx_Val_Volts, None)
+# Assign Vtask to analogue pin
+VTask1.CreateAOVoltageChan("Dev1/ao0","VTask1",0,5,DAQmx_Val_Volts,None) # This is analogue pin 0 of Device 1
+VTask2.CreateAOVoltageChan("Dev1/ao1","VTask2",0,5,DAQmx_Val_Volts, None) # This is analogue pin 1 of Device 1
 
 # Define Functions for digital ON/OFF and Analogue
 
 def SetBitOn(MyBitTask):
-    '''Apply 5V output to a digital pin corresponding to the given task'''
+    '''Apply 5V output to a digital pin corresponding to the provided task'''
     MyBitTask.WriteDigitalLines(1,1,-1,DAQmx_Val_GroupByChannel,Num1,None,None)
     MyBitTask.StopTask() # Task must be stopped in order to be able to play with the other channels/tasks
     
 def SetBitOff(MyBitTask):
-    '''Apply 0V output to a digital pin corresponding to the given task'''    
+    '''Apply 0V output to a digital pin corresponding to the provided task'''    
     MyBitTask.WriteDigitalLines(1,1,-1,DAQmx_Val_GroupByChannel,Num0,None,None)
     MyBitTask.StopTask()
 
 
-def Voltage(MyVTask,Percentage):
-    '''Apply voltage (in % : 100% = 5V) to an analogue pin corresponding to the given task'''
+def Voltage(MyVTask, Percentage):
+    '''Apply voltage (in % : 100% = 5V) to an analogue pin corresponding to the provided task'''
     VoltOut = 5 * Percentage/100
     MyVTask.WriteAnalogScalarF64(1,-1,VoltOut,None)
     # Syntax : WriteAnalogScalarF64 (TaskHandle taskHandle, bool32 autoStart, float64 timeout, float64 value, bool32 *reserved);
@@ -58,7 +58,7 @@ def Voltage(MyVTask,Percentage):
 def Reset():
     '''Set all pin in List_Bit and List_Volt to 0V output'''
     for i in List_Bit:
-        i.WriteDigitalLines(1,1,-1,DAQmx_Val_GroupByChannel,Num0,None,None) # switch off lasers
+        i.WriteDigitalLines(1,1,-1,DAQmx_Val_GroupByChannel,Num0,None,None)
     
     for i in List_Volt:
         i.WriteAnalogScalarF64(1,-1,0,None)
